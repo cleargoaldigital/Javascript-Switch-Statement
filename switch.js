51f146472d193fdc650956a8d28c05b153a20b97
cleargoaldigital <cleargoal.ng@gmail.com>
2021-10-26 15:35:21 +0100

    Javascript Switch Statement

--- switch.js
+++ switch.js
@@ -18,3 +18,39 @@ swich (expression) {
 
     actually, just run this code.
 }*/
+
+const select = document.querySelector('select');
+const comment = document.querySelector('.weatherreport');
+
+select.addEventListener('change', setWeather);
+
+function setWeather() {
+  const choice = select.value;
+ 
+  switch (choice) {
+         case 'sunny':
+          comment.innerHTML = 'The day is sunny. Let\'s go have some fun in the park.';
+          break;
+         case 'rainy':
+            comment.innerHTML = 'It is raining; we can\'t go out today.';
+            break;
+         case 'snowing':
+            comment.innerHTML = 'The snow is so chilling. You had better stay indoor.';
+            break;
+         case 'overcast':
+          comment.innerHTML = 'The sky isn\'t clear enough for safe driving. We can wait till tomorrow';
+          break;
+          default:
+            comment.innerHTML = '';
+        }
+  }
+
+
+//   Tenary Operator
+
+/* This is conditional test that test whether an expression is true or false.
+It is most useful in the instance of YES/NO. Its pseudocode is written as follows:
+
+(condition) ? run this code : run this code instead */
+
+let studentName = (Taofeek) ? 'Yes, he\'s my student.' : 'No, he isn\'t my student.'
