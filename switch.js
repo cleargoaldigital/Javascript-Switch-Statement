6102af75390760138606d5374b30a891b857f0f6
cleargoaldigital <cleargoal.ng@gmail.com>
2021-10-26 14:30:18 +0100

    Javascript Switch Statements
    Lessons in Javascript Switch statement.

---
+++ switch.js
@@ -0,0 +1,20 @@
+/* Swich statements take a single expression/value as an input,
+and then look through a number of choices until
+they find one that matches the stated value, and then run the corresponding code.
+A pseudocode is hereunder written.
+
+
+swich (expression) {
+    case choice1:
+        run this code 
+        break;
+    case choice2:
+        run this code 
+        break;
+    case choice3:
+        run this code 
+        break;
+    // include as many cases as you like.
+
+    actually, just run this code.
+}*/
